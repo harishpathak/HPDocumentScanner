4eba3986281a897a0134a9807208a9c559ce632e
Harish Pathak <harish.pathak@crmnext.in>
2018-08-14 16:01:26 +0530

    EOD Assignment

---
+++ EODAssignment/EODAssignment/Views/MCQViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MCQViewController.swift
+//  EODAssignment
+//
+//  Created by Harish Pathak on 14/08/18.
+//  Copyright © 2018 CRMNEXT. All rights reserved.
+//
+
+import UIKit
+
+class MCQViewController: UIViewController {
+
+    @IBOutlet weak var tblQuestionList: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func actionCheckAnswers(_ sender: Any) {
+    }
+    
+    @IBAction func actionReset(_ sender: Any) {
+    }
+    
+    }
+
